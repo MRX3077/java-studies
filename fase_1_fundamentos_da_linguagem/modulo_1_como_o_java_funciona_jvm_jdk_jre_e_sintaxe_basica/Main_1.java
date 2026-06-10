121a8665aed26fc28182e1250ee076d599dfac88
MRX2077 <igoremailtrabalho123@gmail.com>
2026-06-10 15:09:51 -0300

    Criação do arquivo para printar 'Hello World' no terminal

---
+++ fase_1_fundamentos_da_linguagem/modulo_1_como_o_java_funciona_jvm_jdk_jre_e_sintaxe_basica/Main_1.java
@@ -0,0 +1,7 @@
+package fase_1_fundamentos_da_linguagem.modulo_1_como_o_java_funciona_jvm_jdk_jre_e_sintaxe_basica;
+
+public class Main_1 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    } 
+}
